65134282c6345740f412a2c3e307591af34bd3d8
AsleepRodent <qualitychronology@gmail.com>
2026-06-10 01:33:26 -0600

    fix: remove old local import of History class

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,4 @@
 export { Journal, type JournalSettings } from "./journal/journal.ts";
 export { Handler, type HandlerSettings } from "./journal/handler.ts";
-export { History, type HistorySettings } from "./journal/history.ts";
 export type { Message } from "./common/types/message.ts";
 export { ConsoleHandler } from "./journal/resources/handlers/console.ts";
